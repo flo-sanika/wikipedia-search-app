51baee31d40919ee39eaf42e10145b4b598c845b
Riazzor <flo.sanika+@gmail.com>
2021-01-12 21:46:23 +0100

    fetch done; now to display result.

--- js/main.js
+++ js/main.js
@@ -3,16 +3,36 @@ console.log(typeof(formulaire));
 
 formulaire.addEventListener('submit', search);
 
-function search(event) {
+async function search(event) {
       // prevent page from reloading when form is submitted
       event.preventDefault();
       // get the value of the input field
-      let inputValue = event.currentTarget.elements[0].value.trim();
-      // console.log(inputValue);
+      let searchQuery = event.currentTarget.elements[0].value.trim();
+      let results = "";
+      try {
+            results = await searchWikipedia(searchQuery);
+            
+      } catch (error) {
+            console.log(error);
+            alert("problem in catch line 18");
+      }
+      console.log(results);
+}
+
+async function searchWikipedia(searchQuery) {
+      const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+      let result = await fetch(endpoint)
+            .then(response=> response.json())
+            .catch(error => console.log(error));
+      // console.log(result);
+            
+      return result;
+}
+
+      
+function affichageResultat(resultat) {
 
 }
-      
-      
       
       
       
